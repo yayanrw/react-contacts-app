c385369bab641597c5f499e24f76f602521ec009
yayanrw <yayanraw@gmail.com>
2023-02-13 15:25:07 +0700

    init data

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      name: "Yayan Rahmat Wijaya",
+      tag: "yayanrw",
+      imageUrl: "/images/yayan1.jpg",
+    },
+    {
+      id: 2,
+      name: "Elrawi 27",
+      tag: "elrawi",
+      imageUrl: "/images/yayan2.jpg",
+    },
+    {
+      id: 3,
+      name: "BPOM Nasional",
+      tag: "bpom",
+      imageUrl: "/images/bpom.png",
+    },
+    {
+      id: 4,
+      name: "Dermatology Nasional",
+      tag: "dermatology",
+      imageUrl: "/images/dermatology.png",
+    },
+    {
+      id: 1,
+      name: "Halalan Toyyibah",
+      tag: "halal",
+      imageUrl: "/images/halal.png",
+    },
+  ];
+};
+
+export { getData };
