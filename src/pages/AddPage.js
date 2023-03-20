99250ec63dd0929bd94e176a366d9cf3007a2b12
yayanrw <yayanraw@gmail.com>
2023-03-20 13:57:53 +0700

    redirect after add

--- src/pages/AddPage.js
+++ src/pages/AddPage.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { addContact } from "../utils/data";
 import ContactInput from "../components/ContactInput";
+import { useNavigate } from "react-router-dom";
 
 const AddPage = () => {
+  const navigate = useNavigate();
+
   const onAddContactHandler = (contact) => {
     addContact(contact);
+    navigate("/");
   };
 
   return (
