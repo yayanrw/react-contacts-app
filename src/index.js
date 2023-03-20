eb0173afc61fb811444245f4e8a7092d56204487
yayanrw <yayanraw@gmail.com>
2023-03-20 13:19:38 +0700

    use browser router on index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import ContactApp from "./components/ContactApp";
 
 import "./styles/style.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<ContactApp />);
+root.render(
+  <BrowserRouter>
+    <ContactApp />
+  </BrowserRouter>
+);
