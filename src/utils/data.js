d02634a2a7a797e002299e53e26e5b0120c1cc00
yayanrw <yayanraw@gmail.com>
2023-03-20 13:28:46 +0700

    add more func

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,36 +1,47 @@
-const getData = () => {
-  return [
-    {
-      id: 1,
-      name: "Yayan Rahmat Wijaya",
-      tag: "yayanrw",
-      imageUrl: "/images/yayan1.jpg",
-    },
-    {
-      id: 2,
-      name: "Karang Taruna",
-      tag: "katar",
-      imageUrl: "/images/karangtaruna.png",
-    },
-    {
-      id: 3,
-      name: "BPOM Nasional",
-      tag: "bpom",
-      imageUrl: "/images/bpom.png",
-    },
-    {
-      id: 4,
-      name: "Dermatology Nasional",
-      tag: "dermatology",
-      imageUrl: "/images/dermatology.png",
-    },
-    {
-      id: 5,
-      name: "Halalan Toyyibah",
-      tag: "halal",
-      imageUrl: "/images/halal.png",
-    },
+let contacts = [
+  {
+    id: 1,
+    name: "Yayan Rahmat Wijaya",
+    tag: "yayanrw",
+    imageUrl: "/images/yayan1.jpg",
+  },
+  {
+    id: 2,
+    name: "Karang Taruna",
+    tag: "katar",
+    imageUrl: "/images/karangtaruna.png",
+  },
+  {
+    id: 3,
+    name: "BPOM Nasional",
+    tag: "bpom",
+    imageUrl: "/images/bpom.png",
+  },
+  {
+    id: 4,
+    name: "Dermatology Nasional",
+    tag: "dermatology",
+    imageUrl: "/images/dermatology.png",
+  },
+  {
+    id: 5,
+    name: "Halalan Toyyibah",
+    tag: "halal",
+    imageUrl: "/images/halal.png",
+  },
+];
+
+const getContacts = () => contacts;
+
+const addContact = (contact) => {
+  contacts = [
+    ...contacts,
+    { id: +new Date(), imageUrl: "/images/default.jpg", ...contact },
   ];
 };
 
-export { getData };
+const deleteContact = (id) => {
+  contacts = contacts.filter((contact) => contact.id !== id);
+};
+
+export { getContacts, addContact, deleteContact };
